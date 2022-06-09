f9bc608eeb5e1b552dc13c4ce4613a19123cb1c2
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-06-09 03:37:06 -0400

    Refactored code into the `geometry` submodule

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Document, Page } from "react-pdf"
 import { PDFPageProxy } from "pdfjs-dist/types/src/display/api"
 import PropTypes from "prop-types"
 
+import Geometry, { Point2D } from "./geometry"
+
 import "./App.css"
 
 // @ts-ignore
@@ -11,9 +13,7 @@ import samplePDF from "./k522-venice.pdf"
 import { pdfjs } from "react-pdf"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
-type Point2D = { x: number; y: number }
-
-const originPoint: Point2D = { x: 0, y: 0 }
+const originPoint: Point2D = { x: 0, y: 0 } as Point2D
 
 enum BarCuttingStage {
   Empty = 0,
@@ -171,7 +171,7 @@ class App extends React.Component<Props, State> {
 
       case BarCuttingStage.Cutting:
         // check if the cutting point is beyond the bounding box of the region
-        const pointProjectedTop = projectPointOnLine(
+        const pointProjectedTop = Geometry.projectPointOnLine(
           this.state.topLeftCorner as Point2D,
           this.state.topRightCorner as Point2D,
           { x, y },
@@ -180,7 +180,7 @@ class App extends React.Component<Props, State> {
 
         // this number computes the proportion between the starting boundary of the box and the cutting point
         // this helps us determine whether we have cut all points on the system/staff or not
-        const proportionOfBox = proportionPointOnLine(
+        const proportionOfBox = Geometry.proportionPointOnLine(
           this.state.topLeftCorner as Point2D,
           this.state.topRightCorner as Point2D,
           pointProjectedTop as Point2D
@@ -289,7 +289,7 @@ class App extends React.Component<Props, State> {
     this.drawPoint(ctx, this.state.staffHeightPoint)
 
     //
-    const distance = measureDistance(
+    const distance = Geometry.measureDistance(
       this.state.topLeftCorner,
       this.state.topRightCorner
     )
@@ -299,7 +299,7 @@ class App extends React.Component<Props, State> {
     const dy =
       (this.state.topLeftCorner.y - this.state.topRightCorner.y) / distance
 
-    const height = measureHeightFromPoints(
+    const height = Geometry.measureHeightFromPoints(
       this.state.topLeftCorner,
       this.state.topRightCorner,
       this.state.staffHeightPoint
@@ -328,7 +328,7 @@ class App extends React.Component<Props, State> {
     const projectToLines = (
       point: Point2D
     ): { top: Point2D; bottom: Point2D } | undefined => {
-      const pointProjectedTop = projectPointOnLine(
+      const pointProjectedTop = Geometry.projectPointOnLine(
         this.state.topLeftCorner as Point2D,
         this.state.topRightCorner as Point2D,
         point,
@@ -336,7 +336,7 @@ class App extends React.Component<Props, State> {
       )
       if (!pointProjectedTop) return
       this.drawPoint(ctx, pointProjectedTop)
-      const pointProjectedBottom = projectPointOnLine(
+      const pointProjectedBottom = Geometry.projectPointOnLine(
         bottomLeftCorner as Point2D,
         bottomRightCorner as Point2D,
         point,
@@ -436,184 +436,6 @@ class App extends React.Component<Props, State> {
       </div>
     )
   }
-}
-
-const proportionPointOnLine = (
-  p1: Point2D,
-  p2: Point2D,
-  pProject: Point2D,
-  flip: boolean = false
-) => {
-  // assume all points are on line (otherwise unexpected results!)
-  // p1 and p2 are the end points of the line
-  // pProject is the point to project
-
-  // check?
-  const pProjectPrime = projectPointOnLine(p1, p2, pProject, flip)
-  console.log(
-    pProjectPrime &&
-      pProject.x === pProjectPrime.x &&
-      pProject.y === pProjectPrime.y
-  )
-
-  if (p1.x !== p2.x) return (pProject.x - p1.x) / (p2.x - p1.x)
-  else return (pProject.y - p1.y) / (p2.y - p1.y)
-}
-
-const projectPointOnLine = (
-  p1: Point2D,
-  p2: Point2D,
-  pProject: Point2D,
-  flip: boolean = false
-) => {
-  // p1 x---X-----x p2
-  //        |
-  //        x pProject
-  //
-
-  const distance = measureDistance(p1, p2)
-  if (distance === 0) return
-
-  const dx = (p1.x - p2.x) / distance
-  const dy = (p1.y - p2.y) / distance
-
-  const vecLineP1toP2 = { dx, dy }
-  const vecOrthogonalToLineP1toP2 = {
-    dx: dy * (flip ? -1 : 1),
-    dy: -dx * (flip ? -1 : 1),
-  }
-
-  const height = measureHeightFromPoints(p1, p2, pProject)
-
-  return {
-    x: pProject.x - vecOrthogonalToLineP1toP2.dx * height,
-    y: pProject.y - vecOrthogonalToLineP1toP2.dy * height,
-  }
-}
-
-// SUSPICIOUS CODE
-const measureProportionOnLine = (
-  p1: Point2D,
-  p2: Point2D,
-  pMiddle: Point2D
-) => {
-  //
-  const height = measureHeightFromPoints(p1, p2, pMiddle)
-  const angle = measureAngleFromPoints(p2, pMiddle, p1)
-
-  // we are trying to compute the adjacent side of the triangle
-  // given: (a) hypothenuse (height) and (b) angle
-
-  const segment = height / Math.sin(angle)
-
-  const distance = measureDistance(p1, p2)
-  const proportion = segment / distance
-  console.log(
-    `segment: ${segment} distance: ${distance} proportion: ${proportion}`
-  )
-  return proportion
-}
-
-const measureLineDiff = (p1: Point2D, p2: Point2D) => {
-  const dx = p2.x - p1.x
-  const dy = p2.y - p1.y
-  const length = Math.sqrt(dx * dx + dy * dy)
-  return {
-    dx: dx / length,
-    dy: dy / length,
-    length: length,
-  }
-}
-
-const measureDistance = (p1: Point2D, p2: Point2D) => {
-  return measureLineDiff(p1, p2).length
-}
-
-const measureHeightFromPoints = (
-  p1: Point2D,
-  p2: Point2D,
-  pMiddle: Point2D
-) => {
-  const area = measureTriangleAreaFromPoints(p1, p2, pMiddle)
-  const base = measureDistance(p1, p2)
-  return (2 * area) / base
-}
-
-const measureTriangleAreaFromPoints = (
-  p1: Point2D,
-  p2: Point2D,
-  p3: Point2D
-) => {
-  const a = measureDistance(p1, p2)
-  const b = measureDistance(p2, p3)
-  const c = measureDistance(p3, p1)
-  const s = (a + b + c) / 2
-  return Math.sqrt(s * (s - a) * (s - b) * (s - c))
-}
-
-type degree = number
-type radian = number
-
-// Converts radians to degrees.
-function radianToDegrees(angle: radian): degree {
-  return angle * (180 / Math.PI)
-}
-
-// Converts degrees to radians.
-function degreesToRadian(angle: degree): radian {
-  return angle * (Math.PI / 180)
-}
-
-function getLineAngle(p1: Point2D, p2: Point2D): radian {
-  const dx = p2.x - p1.x
-  const dy = p2.y - p1.y
-  const angle = Math.atan2(dy, dx)
-  return angle
-}
-
-function measureAngleFromPoints(
-  p1: Point2D,
-  p2: Point2D,
-  pMiddle: Point2D
-): radian {
-  var AB = Math.sqrt(
-    Math.pow(pMiddle.x - p1.x, 2) + Math.pow(pMiddle.y - p1.y, 2)
-  )
-  var BC = Math.sqrt(
-    Math.pow(pMiddle.x - p2.x, 2) + Math.pow(pMiddle.y - p2.y, 2)
-  )
-  var AC = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2))
-  return Math.acos((BC * BC + AB * AB - AC * AC) / (2 * BC * AB))
-}
-const measureAngleFromPoints2 = (
-  p1: Point2D,
-  p2: Point2D,
-  pMiddle: Point2D
-) => {
-  // Measure angle in this configuration:
-  //   p1
-  //  /
-  // pMiddle -- p2
-
-  const numerator =
-    p1.y * (pMiddle.x - p2.x) +
-    pMiddle.y * (p2.x - p1.x) +
-    p2.y * (p1.x - pMiddle.x)
-  const denominator =
-    (p1.x - pMiddle.x) * (pMiddle.x - p2.x) +
-    (p1.y - pMiddle.y) * (pMiddle.y - p2.y)
-  const ratio = numerator / denominator
-
-  const angleRad = Math.atan(ratio)
-  //return angleRad
-
-  const angleDeg = (angleRad * 180) / Math.PI
-
-  if (angleDeg < 0) {
-    return angleDeg + 180
-  }
-
-  return angleDeg
 }
 
 export default App
